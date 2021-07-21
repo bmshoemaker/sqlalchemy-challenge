f28c629875b79f7c72892d7e1ff8c95b4abe96a5
Breckin_Shoemaker <breckinshoemaker@gmail.com>
2021-07-21 12:07:26 -0500

    Pushing updated code

---
+++ app.py
@@ -0,0 +1,76 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+station = Base.classes.station
+measurement = Base.classes.measurement
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+# Home page
+@app.route("/")
+# List all routes that are available.
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end"
+    )
+
+
+#Precipitation Route
+#@app.route("/api/v1.0/precipitation")
+# Convert the query results to a dictionary using date as the key and prcp as the value.
+# Return the JSON representation of your dictionary.
+
+# Stations Route
+#@app.route("/api/v1.0/stations")
+#Return a JSON list of stations from the dataset.
+
+#Temperature Route
+#@app.route("/api/v1.0/tobs")
+# Query the dates and temperature observations of the most active station for the last year of data.
+# Return a JSON list of temperature observations (TOBS) for the previous year.
+
+# Start Only Route
+#@app.route("/api/v1.0/<start>")
+# Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
+# When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.
+
+#Start and End Route
+#@app.route("/api/v1.0/<start>/<end>")
+# Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
+# When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive.
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
